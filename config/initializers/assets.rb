14c2a386d883e81a635b0614720c46bde9e4e9f9
Wilian Ferreira <wpaife@gmail.com>
2022-01-05 00:18:59 -0300

    Adjusting users logout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,7 +19,7 @@ Rails.application.config.assets.precompile += %w[admins_backoffice.js admins_bac
 # lib/assets
 Rails.application.config.assets.precompile += %w[sb-admin-2.js sb-admin-2.css
                                                  gentelella.js gentelella.css
-                                                 img.jpg]
+                                                 img.svg]
 
 # vendot/assets
 Rails.application.config.assets.precompile += %w[jquery-2.2.3/dist/jquery.js]
