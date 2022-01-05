8c254e23e39e34f98563a6a9eccd7e6399b50590
Wilian Ferreira <wpaife@gmail.com>
2022-01-04 23:26:50 -0300

    Porting gentelella to users

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 
+# app/assets
+Rails.application.config.assets.precompile += %w[admins_backoffice.js admins_backoffice.css
+                                                 users_backoffice.js users_backoffice.css]
+
 # lib/assets
-Rails.application.config.assets.precompile += %w[admins_backoffice.js admins_backoffice.css]
+Rails.application.config.assets.precompile += %w[sb-admin-2.js sb-admin-2.css
+                                                 gentelella.js gentelella.css]
+
+# vendot/assets
+Rails.application.config.assets.precompile += %w[jquery-2.2.3/dist/jquery.js]
