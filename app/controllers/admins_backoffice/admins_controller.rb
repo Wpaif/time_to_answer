29d0283c144b4f90e8bd9a886ccdda17675de7cc
Wilian Ferreira <wpaife@gmail.com>
2022-01-12 19:30:00 -0300

    Enabling registration of new admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,6 +6,20 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: 'Admin Successfully Created'
+    else
+      render :new
+    end
+  end
+
   def edit; end
 
   def update
