49c67251f851c97fa5a22636fb1eee31bfe615ae
Wilian Ferreira <wpaife@gmail.com>
2022-01-17 20:37:40 -0300

    Enabling registration of new subject

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -2,4 +2,24 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   def index
     @subjects = Subject.all
   end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      redirect_to admins_backoffice_subjects_path, notice: 'Subject Successfully Created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:description)
+  end
 end
