481c4fc785aa1a2361e3e6609601fabab4b64c66
Wilian Ferreira <wpaife@gmail.com>
2022-01-21 16:09:58 -0300

    Incrementing i18n for the models

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+  def translate_attribute(object = nil, method = nil)
+    if object && method
+      object.model.human_attribute_name(method.to_sym)
+    else
+      'Invalid parameters to complete the translation of this attribute'
+    end
+  end
 end
