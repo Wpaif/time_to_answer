3171a81796cf96dc8e944967f448ad231e2165d8
Wilian Ferreira <wpaife@gmail.com>
2022-02-03 18:07:45 -0300

    Changing to ruby 2.7.5 and doing the search works

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,6 @@
 class Site::SearchController < SiteController
   def question
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers).where('lower(description) LIKE ?',
+                                                   "%#{params[:term].downcase}%").page(params[:page])
   end
 end
