8c9c9c99db8aafcfb007224dad48c68760789744
Wilian Ferreira <wpaife@gmail.com>
2022-02-16 01:31:47 -0300

    Updating the user data

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -1,3 +1,33 @@
 class UsersBackoffice::ProfileController < UsersBackofficeController
+  before_action :set_user
+  before_action :check_password, only: %i[update]
+
   def edit; end
+
+  def update
+    if @user.update(params_user)
+      sign_in(@user, bypass: true)
+      redirect_to users_backoffice_profile_path, notice: 'User Successfully Updated'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def params_user
+    params.require(:user).permit(:first_name, :last_name, :emain, :password, :password_confirmation)
+  end
+
+  def check_password
+    if params[:user][:password].blank? &&
+       params[:user][:password_confirmation].blank?
+
+      params[:user].extract!(:password, :password_confirmation)
+    end
+  end
 end
