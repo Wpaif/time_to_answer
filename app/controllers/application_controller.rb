407fc88a9b596bececf49294d8ebe0a2eee7a00e
Wilian Ferreira <wpaife@gmail.com>
2022-02-22 16:51:49 -0300

    Forcing the user to log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :check_pagination
 
   private
 
   def layout_by_resource
     scope_name = resource_class.to_s.downcase.concat('_devise')
     devise_controller? ? scope_name : 'application'
+  end
+
+  def check_pagination
+    params.extract!(:page) unless user_signed_in?
   end
 end
