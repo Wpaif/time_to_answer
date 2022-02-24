395468c3abf737d694eb1fa78927560790cba8f5
Wilian Ferreira <wpaife@gmail.com>
2022-02-23 22:34:48 -0300

    Adding user statistics

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,5 +1,17 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer_id]) if params[:answer_id]
+    assing_user_statistic(@answer)
+  end
+
+  private
+
+  def assing_user_statistic(answer)
+    if user_signed_in?
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+
+      user_statistic.save
+    end
   end
 end
