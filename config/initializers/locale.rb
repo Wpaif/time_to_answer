43ba5cd82d20947d24571458812e6101275c1364
Wilian Ferreira <wpaife@gmail.com>
2022-03-03 18:09:41 -0300

    Adding internationalization to CRUD pages

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,3 @@
-# Whitelist locales available for the application
-I18n.available_locales = [:en, 'pt-BR']
-
-# Set default locale to something other than :en
-I18n.default_locale = :en
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+I18n.available_locales = %i[en pt-BR]
+I18n.default_locale = :'pt-BR'
